df982adf68faa416f807d22b0675bf3091153812
patorma <pocontreras@ing.ucsc.cl>
2021-04-28 01:08:52 -0400

    se termino detalle de un curso

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -11,8 +12,9 @@ class PageController extends Controller
         return view('index');
     }
 
-    public function course(){
-
-        return view('course');
+    public function course(Course $course){
+        // se coloca compact para no escribir un array
+        // cuando la clave y el valor son iguales
+        return view('course',compact('course'));
     }
 }
